50ef31b06915e943cdbb8475ac2e7a1f8d25c3f0
JonathanHuang876200 <jonathan876200@gmail.com>
2017-03-19 16:41:12 +0800

    add doctorusedrecord

---
+++ src/oo/abstruction/DoctorUsedRecord.java
@@ -0,0 +1,18 @@
+package oo.abstruction;
+
+public class DoctorUsedRecord {
+
+	int ChartNumber;
+	String IdentityCard;
+	String name;
+	String Gender;
+	String Birthday;
+	String Phone;
+	String MobilePhone;
+	String Address;
+	/* ==================以下為醫療人員才可以看到的資訊============================= */
+	String Medicine;
+	String Arlagy;
+	String Surgery;
+
+}
