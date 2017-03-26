41a37f2c98e0705b19a8f62aac1bf21fda061706
JonathanHuang876200 <jonathan876200@gmail.com>
2017-03-26 12:52:13 +0800

    2017,3,26 push

--- src/oo/abstruction/Car.java
+++ src/oo/abstruction/Car.java
@@ -10,10 +10,6 @@ public class Car {
 	int id;
 
 	public Car(String brand, int cc, String name, String type) {
-		this.brand = brand;
-		this.cc = cc;
-		this.name = name;
-		this.type = type;
 		
 		
 	}
--- src/oo/abstruction/PatientRecordTester.java
+++ src/oo/abstruction/PatientRecordTester.java
@@ -3,8 +3,13 @@ package oo.abstruction;
 public class PatientRecordTester {
 
 	public static void main(String[] args) {
-		PatientRecord p1 = new PatientRecord(001, "F129968489", "Jonathan", "M", "19980602", "022219xxx", "093215xxxx","New Taipei City");
-		PatientRecord p2 = new PatientRecord(002, "Fxxxxxxxxx", "vivian", "F", "20000416", "022219xxx", "093215xxxx","Taichung");
+
+		PatientRecord[] patientrecords = new PatientRecord[3];
+		patientrecords[0] = new PatientRecord(001, "F129968489", "Jonathan", "M", "19980602", "022219xxx", "093215xxxx",
+				"New Taipei City");
+		patientrecords[1] = new PatientRecord(002, "Fxxxxxxxxx", "vivian", "F", "20000416", "022219xxx", "093215xxxx",
+				"Taichung");
+
 	}
 
 }
