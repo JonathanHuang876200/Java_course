afc8a0573044ab981bea8935d06de2e93339e028
JonathanHuang876200 <jonathan876200@gmail.com>
2017-04-05 22:40:44 +0800

    GiveMeMoney(IntergerTest)

--- src/oo/abstruction/GiveMeTheMoney.java
+++ src/oo/abstruction/GiveMeTheMoney.java
@@ -12,25 +12,28 @@ public class GiveMeTheMoney {
 		m.add("821");
 		m.add("886");
 		m.add("554");
+
 		// String num = s.nextline();
 		// System.out.println("請輸入發票的末三碼:");
 		// System.out.println("請輸入發票的八碼數字:");
 
-		String n = "12345678";
-		System.out.println("原始長度:" + n.length());
-		String sub = n.substring(n.length() - 3);
-		System.out.println(sub);
+		// String n = "12345678";
+		// System.out.println("原始長度:" + n.length());
+		// String sub = n.substring(n.length() - 3);
+		// System.out.println(sub);
 
 		String e = s.nextLine();
+
 		System.out.println("長度:" + e.length());
 		String esub = e.substring(e.length() - 3);
 		System.out.println(esub);
-		// int intsub = s.nextLine();
 
-		if ((esub == m.get(0)) || (esub == m.get(1)) || (esub == m.get(2)) || (esub == m.get(2))) {
-			System.out.println("331中獎");
+		int inte = Integer.parseInt(esub);
+
+		if (inte == 331 || inte == 821 || inte == 886 || inte == 554) {
+			System.out.println("中獎");
 		} else {
-
+			System.out.println("沒中獎");
 		}
 	}
 }
